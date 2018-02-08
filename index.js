1682225daa367dcee6691f069cab7b43411cd273
Sang Woo Moon <swmoonnz@gmail.com>
2018-02-08 01:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeli = []
 
 function takeANumber(katzDeliLine, name) {
-katzDeli.push(name);
+katzDeliLine.push(name);
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
