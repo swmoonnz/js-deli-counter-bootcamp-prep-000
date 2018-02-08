9676a48c7cae224c5fa4aaa49e3ed32d1d2e6bb3
Sang Woo Moon <swmoonnz@gmail.com>
2018-02-08 02:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ if (katzDeli.length === 0)
   }
 }
 
-function currentLine(katzDeliline) {
-  if (katzDeliline.length === 0)
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0)
   {return `The line is currently empty.`}  
   else for(let i = 0; i < line.length; i++) {
-     katzDeli.push(` `+[i+1]+`. `  + line[i])
+     katzDeli.push(` `+[i+1]+`. `  + katzDeliLine[i])
    }
    return(`The line is currently:` + katzDeli)
 }
