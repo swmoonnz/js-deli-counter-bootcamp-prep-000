2e020a502e67a4c181c543a3b97f5563408ada38
Sang Woo Moon <swmoonnz@gmail.com>
2018-02-08 02:29:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (katzDeli.length === 0)
   }
 }
 
-function currentLine(line) {
-  if (line.length === 0)
+function currentLine(katzDeliline) {
+  if (katzDeliline.length === 0)
   {return `The line is currently empty.`}  
   else for(let i = 0; i < line.length; i++) {
      katzDeli.push(` `+[i+1]+`. `  + line[i])
